a360ff881b873c19f62c31861b22c5ab4820c288
ankaijin <147209897+ankaijin@users.noreply.github.com>
2024-03-06 12:15:09 -0800

    initial

---
+++ BruinTour/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BruinTour
+//
+//  Created by Ankai Jin on 3/6/24.
+//
+
+#include <iostream>
+
+int main()
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
